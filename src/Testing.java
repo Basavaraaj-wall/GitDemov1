555d66a012c3303f73aaa97bcc4dc58d9203b67b
walikar <walikar@FOFHMFZF5Q.asiapacific.hpqcorp.net>
2021-10-09 11:03:35 +0530

    First commit....

---
+++ src/Testing.java
@@ -0,0 +1,19 @@
+
+public class Testing {
+	
+	static int add(int a,int b) {
+		int sum=a+b;
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("the sum of two number is : "+Testing.add(3,4));
+		
+
+	}
+
+}
