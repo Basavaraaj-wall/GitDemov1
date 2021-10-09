caaf798fa7906091b7398ff59790b8b0ff5e5b35
walikar <walikar@FOFHMFZF5Q.asiapacific.hpqcorp.net>
2021-10-09 12:53:09 +0530

    committing testing1

---
+++ src/Testing1.java
@@ -0,0 +1,16 @@
+
+public class Testing1 {
+	
+	int Sid;
+	String Sname;
+	
+	public Testing1(int id, String name) {
+		this.Sid=id;
+		this.Sname=name;
+		
+		System.out.println("calling Testing1 constructor");
+	}
+	
+	
+
+}
